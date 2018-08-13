04e16d6645751dce077aad76e35150e778e260fe
KasperAPedersen <5123731@rts-365.dk>
2018-08-13 13:20:11 +0200

    Optimized & removed code

--- inc/styling/js/index.js
+++ inc/styling/js/index.js
@@ -1,28 +1,11 @@
-// Modal Image Gallery
-function onClick(element) {
-  document.getElementById("img01").src = element.src;
-  document.getElementById("modal01").style.display = "block";
-  var captionText = document.getElementById("caption");
-  captionText.innerHTML = element.alt;
-}
+// Changing style of navbar on scroll
+function navStyleChange(){
+    var nav = document.getElementById("navbar");
+    nav.className = (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100 ? ("w3-bar w3-card w3-animate-top w3-white") : (nav.className.replace(" w3-card w3-animate-top w3-white", "")));
+}; window.onscroll = function() { navStyleChange(); };
 
-// Change style of navbar on scroll
-window.onscroll = function() {myFunction()};
-function myFunction() {
-    var navbar = document.getElementById("myNavbar");
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
-    } else {
-        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
-    }
+// Showing navbar for smaller devices (phones etc..)
+function navIconChange() {
+    let x = document.getElementById("smallNav");
+    x.className.indexOf("w3-show") == -1 ? x.className += " w3-show" : x.className = x.className.replace(" w3-show", "");
 }
-
-// Used to toggle the menu on small screens when clicking on the menu button
-function toggleFunction() {
-    var x = document.getElementById("navDemo");
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-    } else {
-        x.className = x.className.replace(" w3-show", "");
-    }
-} 
